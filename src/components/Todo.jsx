cc362f5891bdfcd05b9fb3a7fd944f4e7a03f1e0
shuaibkhan_unitedsol <shuaib.khan@unitedsol.net>
2025-11-06 16:04:23 +0500

    ..

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
+import { MdDeleteForever } from "react-icons/md";
+import { FaRegCheckCircle } from "react-icons/fa";
+
 
 const Todo = () => {
   const [inputVal, setInputVal] = useState("");
   const [task, setTask] = useState([]);
+  const [alertMsg, setAlertMsg] = useState("");
 
   const handleInputChange = (value) => {
     setInputVal(value);
@@ -12,12 +16,13 @@ const Todo = () => {
   const handleFormSubmit = (event) => {
     event.preventDefault();
     if(!inputVal.trim()) return;
-    if(task.includes(inputVal)) return;
+    if(task.includes(inputVal)) return setAlertMsg("Alread Exist") ;
     setTask((prevTask)=> setTask([...prevTask, inputVal]));
    setInputVal("");
   }
   return (
     <section className='todo-container'>
+      {alertMsg && <p>{alertMsg}</p>}
     <header>
       <h1>Todo List</h1>
 
@@ -30,6 +35,23 @@ const Todo = () => {
             <button type='submit' className='todo-btn'>Add Task</button>
           </div>
         </form>
+      </div>
+      <div className="task-list-container">
+        <ul>
+          {
+          task?.map((currentTask, index)=>{
+            return (
+              <li key={index}>
+                <span>{currentTask}</span>
+                <div>
+                <span>{<FaRegCheckCircle/>}</span>
+                <span>{<MdDeleteForever/>}</span>
+                </div>
+              </li>
+            )
+          })
+          }
+        </ul>
       </div>
     </header>
     </section>
